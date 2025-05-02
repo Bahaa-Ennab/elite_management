26b28d4c806d47707deafd18ec08957c6cc36251
BAhaa Enab <bahaenab@gmail.com>
2025-05-02 22:02:36 +0300

    add_patient_page

--- Elite_smile/clinic_app/models.py
+++ Elite_smile/clinic_app/models.py
@@ -33,6 +33,17 @@ class User(models.Model):
     password = models.CharField(max_length=255)
     role = models.CharField(max_length=255, default='patient')
     doctor = models.ForeignKey('self', null=True, blank=True, on_delete=models.SET_NULL, related_name='patients')
+    birth_date = models.DateField(blank=True, null=True)
+    gender = models.CharField(max_length=10, blank=True)  # No need for null=True in CharField
+    social_status = models.CharField(max_length=20, blank=True)
+    id_number = models.CharField(max_length=20, blank=True)
+    phone = models.CharField(max_length=20, blank=True)
+    chronic_disease = models.BooleanField(default=False)
+    takes_medications = models.BooleanField(default=False)
+    drug_allergy = models.BooleanField(default=False)
+    allergy_details = models.TextField(blank=True)
+    dialysis = models.BooleanField(default=False)
+    treatment_plan = models.TextField(blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
@@ -51,16 +62,67 @@ class User(models.Model):
 
         User.objects.create(first_name=first_name,last_name=last_name,email=email,password=password_hash,role=role)
 
+    def edit_user_info(request):
+        patient_id = request.POST.get('patient_id')  # اسم الحقل في الفورم هو pateints_names
+        patient = User.objects.get(id=patient_id)
 
-class Message(models.Model):
-    name=models.CharField(max_length=255)
-    email=models.CharField(max_length=255)
-    message=models.TextField()
-    created_at= models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
-    
-    def send_inquiry(request):
-        name =request.POST['name']
-        email =request.POST['email']
-        message =request.POST['message']
-        Message.objects.create(name=name,email=email,message=message)
+        # بيانات موجودة مسبقًا في patient
+        first_name = patient.first_name
+        last_name = patient.last_name
+        email = patient.email
+        password = patient.password
+        role = patient.role
+
+        doctor_id = request.session['userid']
+        doctor = User.objects.get(id=doctor_id)
+
+        # بيانات مأخوذة من الفورم
+        birth_date = request.POST.get('birthdate')
+        gender = request.POST.get('gender')
+        marital_status = request.POST.get('marital_status')
+        id_number = request.POST.get('id_number')
+        phone = request.POST.get('phone')
+        chronic_disease = request.POST.get('chronic_disease', 'no')
+        current_medication = request.POST.get('current_medication', 'no')
+        drug_allergy = request.POST.get('drug_allergy', 'no')
+        allergy_details = request.POST.get('allergy_details', '')
+        dialysis = request.POST.get('dialysis', 'no')
+        dialysis_details = request.POST.get('dialysis_details', '')
+
+        # تحديث بيانات المريض
+        patient.first_name = first_name
+        patient.last_name = last_name
+        patient.email = email
+        patient.password = password
+        patient.role = role
+        patient.doctor = doctor
+        patient.birth_date = birth_date
+        patient.gender = gender
+        patient.social_status = marital_status
+        patient.id_number = id_number
+        patient.phone = phone
+        patient.chronic_disease = chronic_disease
+        patient.takes_medications = current_medication
+        patient.drug_allergy = drug_allergy
+        patient.allergy_details = allergy_details
+        patient.dialysis = dialysis
+        patient.treatment_plan = dialysis_details
+
+        patient.save()
+
+
+
+
+
+    class Message(models.Model):
+        name=models.CharField(max_length=255)
+        email=models.CharField(max_length=255)
+        message=models.TextField()
+        created_at= models.DateTimeField(auto_now_add=True)
+        updated_at = models.DateTimeField(auto_now=True)
+        
+        def send_inquiry(request):
+            name =request.POST['name']
+            email =request.POST['email']
+            message =request.POST['message']
+            Message.objects.create(name=name,email=email,message=message)
--- Elite_smile/doctor_app/urls.py
+++ Elite_smile/doctor_app/urls.py
@@ -5,10 +5,11 @@ urlpatterns = [
 # path('', views.home, name='doctor_home'),
 path('add_patient', views.add_patient, name='add_patient'),
 path('working_hours', views.working_hours, name='working_hours'),
-path('book_appointment,', views.book_appointment, name='book_appointment'),
+path('book_appointment', views.book_appointment, name='book_appointment'),
 path('all_patients', views.all_patients, name='all_patients_display'),
 path('appointments/<int:id>/edit/', views.edit_appointment, name='edit_appointment'),
 path('appointments/<int:id>/delete/', views.delete_appointment, name='delete_appointment'),
 path('admin_main_page', views.admin_main_page, name='admin_main_page'),
 path('log_out', views.log_out, name='log_out'),
+path('edit_user_info', views.edit_user_info, name='edit_user_info'),
 ]
