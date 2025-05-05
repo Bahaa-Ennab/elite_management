abb4531c393ed66bbaa505fa2230089e9d8e1743
BAhaa Enab <bahaenab@gmail.com>
2025-05-05 17:40:04 +0300

    final_2

---
+++ Elite_smile/doctor_app/static/js/book_appointment.js
@@ -0,0 +1,9 @@
+
+    new TomSelect('#patient_id', {
+        create: false,
+        sortField: {
+            field: "text",
+            direction: "asc"
+        },
+        placeholder: "ابحث عن المريض..."
+    });
